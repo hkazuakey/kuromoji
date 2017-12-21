4d8522b90099fb19416288ad8b4b8a7e97f47f37
Kazuaki Hiraga <kazuaki.a.hiraga@rakuten.com>
2017-12-21 16:56:29 +0900

    Add cli tool for testing

---
+++ kuromoji-cli/src/main/java/org/kazu/kuromoji_cli.java
@@ -0,0 +1,22 @@
+package org.kazu;
+
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
+import java.util.List;
+
+public class kuromoji_cli {
+    public static void main( String[] args ) {
+
+        if (args.length < 1) {
+            System.out.println("You must specify the text for Tokenization!");
+            return;
+        }
+
+        Tokenizer tokenizer = new Tokenizer() ;
+        List<Token> tokens = tokenizer.tokenize(args[0]);
+        for (Token token : tokens) {
+            System.out.println(token.getSurface() + "\t" + token.getAllFeatures());
+        }
+        System.out.println("EOS");
+    }
+}
